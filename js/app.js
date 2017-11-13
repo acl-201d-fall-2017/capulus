901ae36f0c0172db62e27a72645993ef59842e57
Katlyn Tucker <studioquirk@yahoo.com>
2017-11-13 14:53:31 -0800

    tested constructor function for cafes

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ function Cafe( name, address, site, work, menu, comm, src){
 }
 
 //render to search page (image, name, address)
-//render rating date to profile page
+//render rating data to profile page
 
 const starbucks = new Cafe ('Starbucks', '1039 NW Couch St', 'http://www.Starbucks.com', 3, 4, 2, 'img/starbucks.jpeg');
 
