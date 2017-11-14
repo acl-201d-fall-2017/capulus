9ebf0e589210d388cd23e411aa8f7020b1c34a75
CharlyWelch <blwbiology@gmail.com>
2017-11-13 17:28:56 -0800

    create shop instances, structure assets folder

--- js/app.js
+++ js/app.js
@@ -1,38 +1,58 @@
-//constructor function for cafes
+'use strict';
 
 class Cafe {
-    constructor (name, address, site, /*menu, comm,*/ src) {
+    constructor (name, address, site, src) {
         this.name = name;
         this.address = address;
         this.site = site;
-        // this.work = work;
-        // this.menu = menu;
-        // this.community = comm;
         this.src = src;
     }
     rateWork(space, outlets, atmosphere) {
         this.space = space;
         this.outlets = outlets;
         this.atmosphere = atmosphere;
-        this.work = Math.abs((space + outlets + atmosphere) / 3);
+        this.work = Math.ceil((space + outlets + atmosphere) / 3);
     }
     rateMenu(quality, variety, price) {
         this.quality = quality;
         this.variety = variety;
         this.price = price;
-        this.menu = Math.abs((quality + variety + price) / 3);
+        this.menu = Math.ceil((quality + variety + price) / 3);
     }
-    rateComm(kids, corp, neighborhood) {
+    rateComm(kids, local, neighborhood) {
         this.kids = kids;
-        this.corp = corp;
+        this.local = local;
         this.neighborhood = neighborhood;
-        this.comm = Math.abs((kids + corp + neighborhood) / 3);
+        this.comm = Math.ceil((kids + local + neighborhood) / 3);
     }
 }
 
-//render to search page (image, name, address)
-//render rating data to profile page
+const starbucks = new Cafe ('Starbucks', '1039 NW Couch St', 'http://www.Starbucks.com', 'assets/images/starbucks.jpeg');
+starbucks.rateWork(4, 2, 3);
+starbucks.rateMenu(3, 2, 4);
+starbucks.rateComm(0, 0, 2);
 
-const starbucks = new Cafe ('Starbucks', '1039 NW Couch St', 'http://www.Starbucks.com', 'img/starbucks.jpeg');
-starbucks.rateWork (4, 2, 3);
+const pearl = new Cafe ('Pearl Bakery', '102 NW 9th Ave', 'http://pearlbakery.com', 'assets/images/pearl.jpeg');
+pearl.rateWork(3, 1, 5);
+pearl.rateMenu(5, 3, 3);
+pearl.rateComm(3, 5, 3);
 
+const albina = new Cafe ('Albina', '4637 N Albina Ave', 'https://www.facebook.com/pages/Albina-Press-Coffeehouse/152791121399765', 'assets/images/albina.jpeg');
+albina.rateWork (3, 3, 5);
+albina.rateMenu (4, 3, 3);
+albina.rateComm (1, 5, 5);
+
+const rohst = new Cafe ('Rohst', '11254 SE 21st Avenue', 'http://www.rohstcoffee.com', 'assets/images/rohst.jpeg');
+rohst.rateWork (2, 4, 4);
+rohst.rateMenu (4, 5, 4);
+rohst.rateComm (4, 5, 5);
+
+const annas = new Cafe ('Anna Bannanas', '8716 N Lombard St', 'http://www.annabannanas.com', 'assets/images/annas.jpeg');
+annas.rateWork (4, 4, 4);
+annas.rateMenu (4, 3, 3);
+annas.rateComm (5, 5, 5);
+
+const barista = new Cafe ('Barista', '1725 NE Alberta St', 'http://www.baristapdx.com', 'assets/images/barista.jpeg');
+barista.rateWork (2, 2, 3);
+barista.rateMenu (2, 2, 4);
+barista.rateComm (1, 3, 3);
