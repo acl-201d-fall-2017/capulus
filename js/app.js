f14a41c7a5ef88f6b78caf0c84a4cc862b317765
CharlyWelch <blwbiology@gmail.com>
2017-11-13 16:52:30 -0800

    add functions for comm and menu

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //constructor function for cafes
 
 class Cafe {
-    constructor (name, address, site, /*menu, comm,*/ src){
+    constructor (name, address, site, /*menu, comm,*/ src) {
         this.name = name;
         this.address = address;
         this.site = site;
@@ -10,18 +10,29 @@ class Cafe {
         // this.community = comm;
         this.src = src;
     }
-    rateWork(space, outlets, atmosphere){
+    rateWork(space, outlets, atmosphere) {
         this.space = space;
         this.outlets = outlets;
         this.atmosphere = atmosphere;
-        this.work = (space + outlets + atmosphere) / 3;
+        this.work = Math.abs((space + outlets + atmosphere) / 3);
     }
-
+    rateMenu(quality, variety, price) {
+        this.quality = quality;
+        this.variety = variety;
+        this.price = price;
+        this.menu = Math.abs((quality + variety + price) / 3);
+    }
+    rateComm(kids, corp, neighborhood) {
+        this.kids = kids;
+        this.corp = corp;
+        this.neighborhood = neighborhood;
+        this.comm = Math.abs((kids + corp + neighborhood) / 3);
+    }
 }
 
 //render to search page (image, name, address)
 //render rating data to profile page
 
-const starbucks = new Cafe ('Starbucks', '1039 NW Couch St', 'http://www.Starbucks.com', /*3, 4, 2,*/ 'img/starbucks.jpeg');
-starbucks.rateWork (2, 3, 5);
+const starbucks = new Cafe ('Starbucks', '1039 NW Couch St', 'http://www.Starbucks.com', 'img/starbucks.jpeg');
+starbucks.rateWork (4, 2, 3);
 
