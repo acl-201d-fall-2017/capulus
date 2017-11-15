6d5132b80a706c180076a4bf4976c7188b2dbda1
Katlyn Tucker <studioquirk@yahoo.com>
2017-11-14 16:03:14 -0800

    begin render function for search page

--- js/app.js
+++ js/app.js
@@ -30,67 +30,77 @@ class Cafe {
     }
 }
 
+const cafes = [];
+
 const starbucks = new Cafe ('Starbucks', '1039 NW Couch St', 'http://www.Starbucks.com', 'assets/images/starbucks.jpeg');
 starbucks.rateWork(4, 2, 3);
 starbucks.rateMenu(3, 2, 4);
 starbucks.rateComm(0, 0, 2);
 starbucks.rateOverall();
+cafes.push(starbucks);
 
 const pearl = new Cafe ('Pearl Bakery', '102 NW 9th Ave', 'http://pearlbakery.com', 'assets/images/pearl.jpeg');
 pearl.rateWork(3, 1, 5);
 pearl.rateMenu(5, 3, 3);
 pearl.rateComm(3, 5, 3);
 pearl.rateOverall();
+cafes.push(pearl);
 
 const albina = new Cafe ('Albina', '4637 N Albina Ave', 'https://www.facebook.com/pages/Albina-Press-Coffeehouse/152791121399765', 'assets/images/albina.jpeg');
 albina.rateWork (3, 3, 5);
 albina.rateMenu (4, 3, 3);
 albina.rateComm (1, 5, 5);
 albina.rateOverall();
+cafes.push(albina);
 
 const rohst = new Cafe ('Rohst', '11254 SE 21st Avenue', 'http://www.rohstcoffee.com', 'assets/images/rohst.jpeg');
 rohst.rateWork (2, 4, 4);
 rohst.rateMenu (4, 5, 4);
 rohst.rateComm (4, 5, 5);
 rohst.rateOverall();
+cafes.push(rohst);
 
 const annas = new Cafe ('Anna Bannanas', '8716 N Lombard St', 'http://www.annabannanas.com', 'assets/images/annas.jpeg');
 annas.rateWork (4, 4, 4);
 annas.rateMenu (4, 3, 3);
 annas.rateComm (5, 5, 5);
 annas.rateOverall();
+cafes.push(annas);
 
 const barista = new Cafe ('Barista', '1725 NE Alberta St', 'http://www.baristapdx.com', 'assets/images/barista.jpeg');
 barista.rateWork (2, 2, 3);
 barista.rateMenu (2, 2, 4);
 barista.rateComm (1, 3, 3);
 barista.rateOverall();
-
-
-// let userName;
-// let newUser;
+cafes.push(barista);
 
 if (document.getElementById('index') != null) {
     const landingForm = document.getElementById ('landing-form');
 
     landingForm.addEventListener('submit', function(){
-        let userName = document.getElementById('username').value;
+        const userName = document.getElementById('username').value;
         localStorage.setItem('username', JSON.stringify(userName));
         //'username' is the key, therefore must be used to access past this point
     });
 }
 
-
-
-
+//This will render the username onto the search page
 if (document.getElementById('search') != null) {
-
-    let newUser = JSON.parse(localStorage.username);
-    //This will render the username onto the search page
+    const newUser = JSON.parse(localStorage.username);
     const greeting = document.getElementById('greeting');
     greeting.textContent = 'Hello, ' + newUser + '. Find your space.';
 }
 
+//Render cafes on SEARCH page
+if (document.getElementById('search') != null) {
+    for ( let i = 0; i < cafes.length; i++){
+        const searchPage = document.getElementById('thumbnail-wrapper');
+        const cafeBlock = document.createElement('div');
+        cafeBlock.setAttribute('class', 'cafeBlock');
+        cafeBlock.style.backgroundImage = url(cafes[i].src);
+        searchPage.appendChild(cafeBlock);
+    }
+}
 
 
 
